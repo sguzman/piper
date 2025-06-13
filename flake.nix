30e14591476af2a5d7808b7f348e7c7ad053cdbb
Salvador Guzman <guzmansalv@gmail.com>
2025-06-13 19:57:20 +0000

    flaky

--- flake.nix
+++ flake.nix
@@ -1,61 +1,43 @@
 {
-  description = "A Nix flake for building the piper TTS package from local sources";
+  description = "A Nix flake for building the piper TTS package using default.nix";
 
   inputs = {
-    # Pinning nixpkgs to a specific version for reproducibility.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      # A list of systems to support. You can add or remove systems here.
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+  outputs = { self, nixpkgs, flake-utils }:
+    # Use flake-utils to simplify supporting multiple systems
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # Import nixpkgs for the specific system
+        pkgs = import nixpkgs { inherit system; };
 
-      # A helper function to generate outputs for each supported system.
-      forEachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+        # Import the package from your default.nix file
+        piper-import = pkgs.callPackage ./default.nix { };
+      in
+      {
+        # The 'default' package is built when you run `nix build`
+        packages.default = piper-import.overrideAttrs (oldAttrs: {
+          # This is the key change: instead of fetching from GitHub,
+          # we use the local source code in the current directory.
+          src = ./.;
 
-    in
-    {
-      # `packages` defines the buildable outputs of the flake.
-      packages = forEachSystem (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-
-          # Import the package definition from your default.nix.txt
-          piperPackageOriginal = pkgs.callPackage ./default.nix.txt { };
-
-        in
-        {
-          # This overrides the `src` attribute of the original package
-          # to point to your local source code directory instead of fetching
-          # it from GitHub.
-          piper = piperPackageOriginal.overrideAttrs (oldAttrs: {
-            # Use the local './src' directory as the source.
-            src = ./src;
-
-            # The original derivation has a `hash` for the fetched source.
-            # We set it to null because we are using local files.
-            # Nix's purity checks are still maintained as the source
-            # is now the flake itself.
-            hash = null;
-          });
+          # The original derivation has a `hash` for the fetched source.
+          # We remove it because we are using local, trusted files.
+          hash = null;
+          
+          # We can also override the version to indicate a local build
+          version = "local-dev";
         });
 
-      # The `defaultPackage` is a convenient alias for `nix build` and `nix run`.
-      defaultPackage = forEachSystem (system: self.packages.${system}.piper);
+        # Default package for `nix run`
+        defaultPackage = self.packages.${system}.default;
 
-      # `devShells` defines development environments.
-      devShells = forEachSystem (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          # This default shell provides all the build-time dependencies
-          # required to work on the piper package.
-          default = pkgs.mkShell {
-            # Pulls in all build inputs from the piper derivation.
-            inputsFrom = [ self.packages.${system}.piper ];
-          };
-        });
-    };
+        # Development shell for `nix develop`
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.default ];
+        };
+      });
 }
+
